d45e2d17ac33a926c06feaf390a795c44fdace23
曾丹 <>
2021-02-01 20:50:20 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello word
+      </div>
+    )
+  }
+}
+export default App;
