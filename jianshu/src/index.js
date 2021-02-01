25e930ebecebcdcb55ab6276d0e61dd87f72c2fa
曾丹 <>
2021-02-01 20:47:38 +0800

    mesage

---
+++ jianshu/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+
+//jsx语法，如果在react项目中使用了jsx语法，就必须引入React 否则会报错。
+ReactDOM.render(<TodoList />, document.getElementById('root'));
+
